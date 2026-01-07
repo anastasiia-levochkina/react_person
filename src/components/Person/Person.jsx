6aacddb9b823978ee4d2fcad526ee3588100032a
Anastasiia Levochkina <nastya.mixaylova98@gmail.com>
2026-01-07 06:12:47 +0200

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,25 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  let partnerText = '';
+
+  if (!isMarried) {
+    partnerText = 'I am not married';
+  } else {
+    const partnerWord = sex === 'm' ? 'wife' : 'husband';
+
+    partnerText = `${partnerName} is my ${partnerWord}`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+
+      {age !== undefined && <p className="Person__age">I am {age}</p>}
+
+      <p className="Person__partner">{partnerText}</p>
+    </section>
+  );
+};
